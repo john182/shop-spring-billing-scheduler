4e90b1a28595575fddb17cd151acb422762d2883
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-04-26 11:33:22 -0300

    add projeto de billing-scheduler

---
+++ src/main/java/com/shop/billing/scheduler/BillingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.shop.billing.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/billing/scheduler/BillingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.billing.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
