1a3d1bee419e75cbdba6ba69388ac06e3ec65222
JohnVanderson <john.vanderson@nfe.com>
2026-06-26 11:09:32 -0300

    Aprimorando respostas da API de Customer

--- src/main/java/com/shop/billing/scheduler/BillingSchedulerApplication.java
+++ src/main/java/com/shop/billing/scheduler/BillingSchedulerApplication.java
@@ -3,10 +3,13 @@ package com.shop.billing.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BillingSchedulerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(BillingSchedulerApplication.class, args);
 	}
 
